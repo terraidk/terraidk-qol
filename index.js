09313c636b05cfdd8e25c24f7f35082b0ca60cd4
Emir Algan <emiralgan2006@gmail.com>
2025-08-20 23:18:34 +0200

    v0.2.1.2

--- index.js
+++ index.js
@@ -10,10 +10,40 @@ import "./menus/shortcuts";
 
 import { PREFIX } from "./utils/constants";
 
-// Read local version from metadata.json
+function getModuleFolder() {
+    const File = Java.type("java.io.File");
+    const modulesDir = new File("config/ChatTriggers/modules/");
+    const folders = modulesDir.listFiles();
+    for (let i = 0; i < folders.length; i++) {
+        const folder = folders[i];
+        if (!FileLib.isDirectory(folder)) {
+            continue;
+        }
+        const file = `${folder}/metadata.json`;
+        if (!FileLib.exists(file)) {
+            continue;
+        }
+        let metadata;
+        try {
+            metadata = JSON.parse(FileLib.read(file));
+        } catch (e) {
+            continue;
+        }
+        if (metadata.name !== "terraidk's QoL") {
+            continue;
+        }
+        return folder;
+    }
+    return null;
+}
+
 let LOCAL_VERSION = "unknown";
 try {
-    const localMeta = FileLib.read("config/ChatTriggers/modules/terraidk-qol/metadata.json");
+    const moduleFolder = getModuleFolder();
+    if (!moduleFolder) {
+        throw new Error("This should not happen");
+    }
+    const localMeta = FileLib.read(`${moduleFolder}/metadata.json`);
     if (localMeta) {
         LOCAL_VERSION = JSON.parse(localMeta).version || "unknown";
     }
@@ -31,22 +61,28 @@ setTimeout(() => {
 }, 500)
 
 function checkForUpdate() {
-    new Thread(() => {
-        try {
-            const content = FileLib.getUrlContent(LATEST_VERSION_URL);
-            let latestVersion = null;
-            if (content) {
-                latestVersion = JSON.parse(content).version;
-            }
-            if (latestVersion && latestVersion !== LOCAL_VERSION) {
-                ChatLib.chat(
-                    new TextComponent("&cA new version of &aTQoL&c is available! &7[&eClick to update&7]")
-                        .setClick("open_url", "https://github.com/terraidk/terraidk-qol/releases/latest")
-                        .setHover("show_text", "&aClick to download the latest version\n&7Current: &c" + LOCAL_VERSION + "\n&7Latest: &a" + latestVersion)
-                );
-            }
-        } catch (e) {
-        }
-    }).start();
+  new Thread(() => {
+    try {
+      const content = FileLib.getUrlContent(LATEST_VERSION_URL)
+      let latestVersion = null
+
+      if (content) {
+        latestVersion = JSON.parse(content).version
+      }
+
+      if (latestVersion && latestVersion !== LOCAL_VERSION) {
+        Client.scheduleTask(() =>
+          ChatLib.chat(
+            new TextComponent('&cA new version of &aTQoL&c is available! &7[&eClick to update&7]')
+              .setClick('open_url', 'https://github.com/terraidk/terraidk-qol/releases/latest')
+              .setHover(
+                'show_text',
+                '&aClick to download the latest version\n&7Current: &c' + LOCAL_VERSION + '\n&7Latest: &a' + latestVersion
+              )
+          )
+        )
+      }
+    } catch (e) {}
+  }).start()
 }
-setTimeout(checkForUpdate, 2000);
+setTimeout(checkForUpdate, 2000)
